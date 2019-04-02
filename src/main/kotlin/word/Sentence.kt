99e48f8698ffcce3879ab11785e4088e84c42c34
Isaac <isaacbncl@gmail.com>
2019-04-03 01:35:47 +0200

    initial commigt

---
+++ src/main/kotlin/word/Sentence.kt
@@ -0,0 +1,21 @@
+package word
+
+class Sentence() {
+
+    lateinit var body: String
+
+    companion object{
+        const val WORDS = "words"
+        const val LETTERS = "letters"
+    }
+
+    fun words():Int {
+        return body.split(" ").count()
+    }
+
+    fun letters():Int {
+        return body.length
+    }
+
+
+}
---
+++ src/main/kotlin/word/WordsController.kt
@@ -0,0 +1,22 @@
+package word
+
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+
+
+@RestController
+class WordsController {
+
+    @PostMapping("/words")
+    fun words(@RequestBody sentence: Sentence): Any {
+         return Collections.singletonMap(Sentence.WORDS, sentence.words())
+    }
+
+    @PostMapping("/letters")
+    fun letters(@RequestBody sentence: Sentence): Any {
+        return Collections.singletonMap(Sentence.LETTERS, sentence.letters())
+    }
+
+}
